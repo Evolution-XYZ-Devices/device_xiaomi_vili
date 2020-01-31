7bda8785c7263928610c16cec5a68456a03a0427
Satya Durga Srinivasu Prabhala <satyap@codeaurora.org>
2020-01-31 11:34:15 -0800

    sm8350-common: Add UClamp support for lahaina
    Sched Tune is deprecated in 5.4 Kernels and is replaced with
    UClamp. As Android releases has to support earlier Kernel versions
    like 4.14, UClamp isn't made default yet.
    
    Change-Id: If6f4ae0812ae456089273514de58bb0654bb08c7

--- common.mk
+++ common.mk
@@ -442,6 +442,9 @@ PRODUCT_PACKAGES += \
     vendor.qti.hardware.perf@2.3.vendor
 
 PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/configs/task_profiles.json:$(TARGET_COPY_OUT_VENDOR)/etc/task_profiles.json
+
+PRODUCT_COPY_FILES += \
     vendor/qcom/opensource/power/config/lahaina/powerhint.xml:$(TARGET_COPY_OUT_VENDOR)/etc/powerhint.xml
 
 # QTI
