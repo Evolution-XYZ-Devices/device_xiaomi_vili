ad089463b1080e7543d593d74dfa7e0f9f20cb1b
Adithya R <gh0strider.2k18.reborn@gmail.com>
2021-12-31 13:06:23 +0530

    vili: rootdir: Initial performance tuning
     * setup runtime cpusets and cpu input boost
    
    Signed-off-by: saikiran2001 <bjsaikiran@gmail.com>
    Signed-off-by: ralph950412 <ralph950412@gmail.com>
    Signed-off-by: blazerpaul15 <blazerpaul15@gmail.com>

--- device.mk
+++ device.mk
@@ -289,6 +289,7 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/rootdir/etc/init.qcom.usb.rc:$(TARGET_COPY_OUT_VENDOR)/etc/init/hw/init.qcom.usb.rc \
     $(LOCAL_PATH)/rootdir/etc/init.qti.kernel.rc:$(TARGET_COPY_OUT_VENDOR)/etc/init/hw/init.qti.kernel.rc \
     $(LOCAL_PATH)/rootdir/etc/init.target.rc:$(TARGET_COPY_OUT_VENDOR)/etc/init/hw/init.target.rc \
+    $(LOCAL_PATH)/rootdir/etc/init.vili.perf.rc:$(TARGET_COPY_OUT_VENDOR)/etc/init/init.vili.perf.rc \
     $(LOCAL_PATH)/rootdir/etc/init.xiaomi.rc:$(TARGET_COPY_OUT_VENDOR)/etc/init/hw/init.xiaomi.rc
 
 PRODUCT_COPY_FILES += \
