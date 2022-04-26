52072438efd9c6e9096c1fa0fd7420c875e1091a
Jyotiraditya Panda <jyotiraditya@aospa.co>
2022-04-25 20:56:35 -0400

    sm8350-common: Do not configure RPC threadpool in camera HAL
     * Hex edit the blob to remove calls to configureRpcThreadpool
       function, by executing NOP
    
     * These calls kill camera with
       Abort message: 'Binder threadpool cannot be shrunk after starting'
    
    Signed-off-by: Jyotiraditya Panda <jyotiraditya@aospa.co>
    Signed-off-by: Adithya R <gh0strider.2k18.reborn@gmail.com>
    Change-Id: If81c3e67a695faae9e14affa66319cb63da4399d
    Signed-off-by: saikiran2001 <bjsaikiran@gmail.com>
    Signed-off-by: ThatMalayaliDeb <theoverpoweredguy@gmail.com>

--- extract-files.sh
+++ extract-files.sh
@@ -99,8 +99,7 @@ function blob_fixup() {
             mv "${EXTRACT_TMP_DIR}/${1##*/}" "${2}"
             ;;
         vendor/lib64/hw/camera.xiaomi.so)
-            hexdump -ve '1/1 "%.2X"' "${2}" | sed "s/5E070094881640F9/1F2003D5881640F9/g; s/AA060094881640F9/1F2003D5881640F9/g" | xxd -r -p > "${EXTRACT_TMP_DIR}/${1##*/}"
-            mv "${EXTRACT_TMP_DIR}/${1##*/}" "${2}"
+            "${SIGSCAN}" -p "4d 07 00 94" -P "1F 20 03 D5" -f "${2}"
             ;;
     esac
 }
