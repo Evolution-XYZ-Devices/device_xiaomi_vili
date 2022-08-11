32ff5209a77e5742b9e1eff7427cdcbb44757c2a
Akash Srivastava <akashniki@gmail.com>
2022-08-11 16:36:43 +0000

    sm8350-common: Don't patch c2 media manifest for ozoaudio

--- extract-files.sh
+++ extract-files.sh
@@ -67,9 +67,6 @@ function blob_fixup() {
         system_ext/lib64/libwfdnative.so)
             "${PATCHELF}" --remove-needed "android.hidl.base@1.0.so" "${2}"
             ;;
-        vendor/etc/vintf/manifest/c2_manifest_vendor.xml)
-            sed -ni '/ozoaudio/!p' "${2}"
-            ;;
         vendor/lib64/android.hardware.secure_element@1.0-impl.so)
             "${PATCHELF}" --remove-needed "android.hidl.base@1.0.so" "${2}"
             ;;
