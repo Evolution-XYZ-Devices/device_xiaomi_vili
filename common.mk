441e2017135bf41c13d0acfeceea3389bb9d9cfd
Akash Srivastava <akashniki@gmail.com>
2022-10-29 04:58:18 +0700

    sm8350-common: Build some missing LDAC libs
    * Bluetooth libs look for them

--- common.mk
+++ common.mk
@@ -97,7 +97,10 @@ PRODUCT_PACKAGES += \
 
 # Bluetooth
 PRODUCT_PACKAGES += \
-    audio.bluetooth.default
+    audio.bluetooth.default \
+    libldacBT_abr \
+    libldacBT_bco \
+    libldacBT_enc
 
 PRODUCT_PACKAGES += \
     android.hardware.bluetooth@1.0.vendor \
