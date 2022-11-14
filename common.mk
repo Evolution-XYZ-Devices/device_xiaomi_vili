85a5da3025e4815a9271255b16d93ffd3daf4205
Joey Huab <joey@evolution-x.org>
2022-11-14 07:51:03 +0000

    sm8350-common: Switch to GCamGOPrebuilt-V3

--- common.mk
+++ common.mk
@@ -124,6 +124,7 @@ PRODUCT_PACKAGES_DEBUG += \
 
 # Camera
 PRODUCT_PACKAGES += \
+    GCamGOPrebuilt-V3 \
     android.hardware.camera.provider@2.4-impl \
     android.hardware.camera.provider@2.4-service_64 \
     vendor.qti.hardware.camera.device@1.0.vendor \
