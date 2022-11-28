c98a3b04db92ff0efd2c43c163135a2d1ed8234e
chemusqui1 <61394758+chemusqui1@users.noreply.github.com>
2022-11-28 16:54:58 +0100

    vili: Add back modules.load file

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -119,6 +119,7 @@ BOOT_KERNEL_MODULES := \
   msm_drm.ko \
   xiaomi_touch.ko
 
+BOARD_VENDOR_KERNEL_MODULES_LOAD := $(strip $(shell cat $(DEVICE_PATH)/modules.load))
 BOARD_VENDOR_RAMDISK_RECOVERY_KERNEL_MODULES_LOAD := $(BOOT_KERNEL_MODULES)
 
 # LiveDisplay
