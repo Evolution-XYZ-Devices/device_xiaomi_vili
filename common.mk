b0b8c74372270d5f5c184f14149ebc0d96c9d2e9
IPSBHANGU <inderpreetbhangu2@gmail.com>
2023-01-20 12:55:44 +0800

    sm8350-common: Import AOSP fingerprint HIDL service * from: https://github.com/LineageOS/android_device_xiaomi_sm8250-common/tree/lineage-18.1/biometrics
    * Rebrand to vili

--- common.mk
+++ common.mk
@@ -192,6 +192,10 @@ PRODUCT_PACKAGES += \
     fastbootd
 
 # Fingerprint
+PRODUCT_PACKAGES += \
+    android.hardware.biometrics.fingerprint@2.3-service.xiaomi_vili \
+    vendor.xiaomi.hardware.fingerprintextension@1.0.vendor
+
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.fingerprint.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.fingerprint.xml
 
