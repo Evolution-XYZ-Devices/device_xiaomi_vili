4e811d7c49abc102a32b0707b6aeccf117a5a2a8
kubersharma001 <kubersharma001@gmail.com>
2023-06-03 10:58:52 +0300

    sm8350-common: Import powerhint.json from coral
    * From: https://android.googlesource.com/device/google/coral/+/android-11.0.0_r42/powerhint.json

--- common.mk
+++ common.mk
@@ -428,6 +428,9 @@ PRODUCT_PACKAGES += \
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/task_profiles.json:$(TARGET_COPY_OUT_VENDOR)/etc/task_profiles.json
 
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/configs/powerhint.json:$(TARGET_COPY_OUT_VENDOR)/etc/powerhint.json
+
 # QTI
 PRODUCT_PACKAGES += \
     libqti_vndfwk_detect.vendor
