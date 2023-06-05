3f84be287cfab5338b947a1212c1e21125a735b6
chaptsand <chaptsand@gmail.com>
2023-06-06 00:21:43 +0300

    sm8350-common: Build Pixel AIDL power HAL

--- common.mk
+++ common.mk
@@ -418,7 +418,7 @@ PRODUCT_USE_DYNAMIC_PARTITIONS := true
 
 # Power
 PRODUCT_PACKAGES += \
-    android.hardware.power-service-qti \
+    android.hardware.power-service.lahaina-libperfmgr \
     android.hardware.power@1.2.vendor \
     vendor.qti.hardware.perf@2.3.vendor
 
@@ -468,7 +468,9 @@ PRODUCT_COPY_FILES += \
 # Soong namespaces
 PRODUCT_SOONG_NAMESPACES += \
     $(LOCAL_PATH) \
-    hardware/xiaomi
+    hardware/xiaomi \
+    hardware/google/interfaces \
+    hardware/google/pixel
 
 # Telephony
 PRODUCT_PACKAGES += \
