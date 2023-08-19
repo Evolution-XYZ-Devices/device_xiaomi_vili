0d65a8d77562c69592e10b01fff3220578e854f6
blazerpaul15 <blazerpaul15@gmail.com>
2023-08-19 21:15:41 +0900

    vili: Add retarded buildbot maintainer

--- device.mk
+++ device.mk
@@ -411,7 +411,8 @@ PRODUCT_COPY_FILES += \
 
 # Overlays
 DEVICE_PACKAGE_OVERLAYS += \
-    $(LOCAL_PATH)/overlay
+    $(LOCAL_PATH)/overlay \
+    $(LOCAL_PATH)/overlay-evolution
 
 PRODUCT_ENFORCE_RRO_TARGETS := *
 
