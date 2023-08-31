a77716dd78ea17296c13e120bf53c1f2c29e0c42
blazerpaul15 <blazerpaul15@gmail.com>
2023-09-01 08:49:43 +0900

    vili: livedisplay: Fix hbm path

--- livedisplay/SunlightEnhancement.cpp
+++ livedisplay/SunlightEnhancement.cpp
@@ -29,7 +29,7 @@ namespace V2_1 {
 namespace implementation {
 
 static constexpr const char* kHbmStatusPath =
-        "/sys/devices/platform/soc/soc:qcom,dsi-display-primary/hbm_enabled";
+        "/sys/devices/platform/soc/soc:qcom,dsi-display-primary/hbm";
 
 Return<bool> SunlightEnhancement::isEnabled() {
     std::string buf;
