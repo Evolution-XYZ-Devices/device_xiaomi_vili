15a588648eb51d019e463b4c50ef0347da41ff31
blazerpaul15 <blazerpaul15@gmail.com>
2023-09-08 23:20:07 +0900

    vili: Drop duplicate inherit products
    *Already here https://github.com/blazerpaul15/vili-device/blob/dd70d452afd81960d930442ef89bde0ddbf37998/evolution_vili.mk#L7-L9

--- device.mk
+++ device.mk
@@ -4,10 +4,6 @@
 # SPDX-License-Identifier: Apache-2.0
 #
 
-# Inherit from those products. Most specific first.
-$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
-$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
-
 # Enable updating of APEXes
 $(call inherit-product, $(SRC_TARGET_DIR)/product/updatable_apex.mk)
 
